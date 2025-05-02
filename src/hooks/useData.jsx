0c6a6bb9a717aa9cc1984659192411c84053b0cb
Steno-95 <lezzistefano95@gmail.com>
2025-05-02 23:16:25 +0200

    fixed url for deployment

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -5,7 +5,7 @@ function useData() {
 
   useEffect(() => {
     async function getData() {
-      const res = await fetch("http://localhost:5173/data.json");
+      const res = await fetch("http://time-tracking-dashboard-f-m/data.json");
       if (!res.ok) return console.log("couldn't fetch data");
       const data = await res.json();
       setContent(data);
