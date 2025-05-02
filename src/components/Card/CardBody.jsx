300595585124926679840bfe32e83e6a0b1d3efe
Steno-95 <lezzistefano95@gmail.com>
2025-05-02 22:49:55 +0200

    small fix

--- src/components/Card/CardBody.jsx
+++ src/components/Card/CardBody.jsx
@@ -18,17 +18,19 @@ function CardBody({ content, time }) {
     <div className="grid grid-cols-2 bg-(--card-bg) px-5 py-8 h-fit rounded-t-xl absolute w-full bottom-0 z-2 items-center justify-center hover:bg-(--hover-color) sm:py-5 sm:h-[12rem] ">
       <h2 className="text-(--active-text) font-medium">{title}</h2>
 
-      <svg
-        width="21"
-        height="5"
-        xmlns="http://www.w3.org/2000/svg"
-        className="fill-(--active-text) hover:fill-white hover:scale-110 justify-self-end cursor-pointer"
-      >
-        <path
-          d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
-          fillRule="evenodd"
-        />
-      </svg>
+      <button>
+        <svg
+          width="21"
+          height="5"
+          xmlns="http://www.w3.org/2000/svg"
+          className="fill-(--active-text) hover:fill-white hover:scale-110 justify-self-end cursor-pointer"
+        >
+          <path
+            d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+            fillRule="evenodd"
+          />
+        </svg>
+      </button>
       <p className="text-(--active-text) font-light text-[1.5rem] sm:text-[3rem] sm:col-start-1 sm:col-end-2 sm:">
         {timeframes[time].current}hrs
       </p>
